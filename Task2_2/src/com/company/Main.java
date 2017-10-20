ffa2df45730bb9c5f5c16ee00d9b92ce652608ec
EkaterinaSvitina <32013989+EkaterinaSvitina@users.noreply.github.com>
2017-10-20 14:34:53 +0300

    Add files via upload

---
+++ Task2_2/src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите номер дня недели: ");
+        int day = in.nextInt();
+
+        switch(day)
+        {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                break;
+            default :
+                System.err.println("Введите корректное значение!");
+                break;
+        }
+
+    }
+
+            }
+
+
+
